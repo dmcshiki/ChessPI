1cee5244cbffa8d3ec420d60de3d002a8d638145
Daniel McCarthy <dmcshiki@gmail.com>
2022-03-02 22:38:02 -0300

    att

--- python.py
+++ python.py
@@ -1,22 +1,37 @@
 import chess
+import chess.svg
+from sqlalchemy import false, true
 
-def Movement(board):
+
+def endGame(board):
     
-
- if(not board.is_checkmate() or not board.is_stalemate() 
+    if(not board.is_checkmate() or not board.is_stalemate() 
     or not board.is_insufficient_material()
-    or not board.is_game_over()):
-    print('Jogadas disponiveis: ', board.legal_moves)
-    move = input ("Digite sua jogada: ")
+    or not board.is_game_over()):    
+        return false
+    else: 
+        return true
 
-    try:
-        board.push_san(move)
-        print(board)
-    except:
-        Movement()
+def Play(board):
+
+    while(endGame(board)):
+        moves = str(board.legal_moves)
+        startingMoves = int(moves.index("(")) 
+        endingMoves = int(moves.index(")"))
+        movementList = list(moves[startingMoves+1:endingMoves].split(" "))
+        print('Jogadas disponiveis: ')
+        for move in movementList:
+            print(move)
+        movement = input ("Digite sua jogada: ")
+        try:
+            board.push_san(movement)
+            print(board)
+        except:
+            print('Movimento invalido, tente novamente.')
+            Play(board)
 
 
 
-
+print(chess.svg.piece(chess.Piece.from_symbol("R")))
 board = chess.Board()
-Movement(board)
+Play(board)
