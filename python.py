683f4063b4f29bf9ec6eb8d1a4e8d60c9efd2d39
Lucas Henrique <61765824+LucasHCarvalho@users.noreply.github.com>
2022-03-03 19:51:54 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,37 +1,35 @@
 import chess
-import chess.svg
-from sqlalchemy import false, true
+
+def endGame():
+
+    return not board.is_checkmate(
+      ) or not board.is_stalemate(
+      ) or not board.is_insufficient_material(
+      ) or not board.is_game_over()
+
+def transformList():
+  moves = str(board.legal_moves)
+  startingMoves = int(moves.index("("))
+  endingMoves = int(moves.index(")"))
+  movementList = list(moves[startingMoves + 1:endingMoves].split(" "))
+  print('Jogadas disponiveis: ')
+  for move in movementList:
+      print(move)
 
 
-def endGame(board):
-    
-    if(not board.is_checkmate() or not board.is_stalemate() 
-    or not board.is_insufficient_material()
-    or not board.is_game_over()):    
-        return false
-    else: 
-        return true
+def Play():
 
-def Play(board):
-
-    while(endGame(board)):
-        moves = str(board.legal_moves)
-        startingMoves = int(moves.index("(")) 
-        endingMoves = int(moves.index(")"))
-        movementList = list(moves[startingMoves+1:endingMoves].split(" "))
-        print('Jogadas disponiveis: ')
-        for move in movementList:
-            print(move)
-        movement = input ("Digite sua jogada: ")
-        try:
-            board.push_san(movement)
-            print(board)
-        except:
-            print('Movimento invalido, tente novamente.')
-            Play(board)
-
-
-
-print(chess.svg.piece(chess.Piece.from_symbol("R")))
+    while (endGame()):
+      transformList()
+      movement = input("Digite sua jogada: ")
+      try:
+        board.push_san(movement)
+        print(" ")
+        print(board)
+          
+      except:
+        print('Movimento invalido, tente novamente.')
+        Play(board)
+        
 board = chess.Board()
-Play(board)
+Play()
