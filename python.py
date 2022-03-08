93ccf9d489923157b72b597cf8f6a3775f68e798
Lucas Henrique <61765824+LucasHCarvalho@users.noreply.github.com>
2022-03-08 09:08:14 -0300

    Update python.py

--- python.py
+++ python.py
@@ -6,14 +6,30 @@ def endGame():
     ) or board.is_insufficient_material(
     ) or board.is_game_over()
 
+def score(board):
+  points = 0
+  
+  #Pontuação peão
+  points = len(board.pieces(chess.PAWN, chess.BLACK)) - len(board.pieces(chess.PAWN, chess.WHITE))
+  #Pontuação cavalo
+  points += 3 * (len(board.pieces(chess.KNIGHT, chess.BLACK)) - len(board.pieces(chess.KNIGHT, chess.WHITE)))
+  #Pontuação bispo
+  points += 3 * (len(board.pieces(chess.BISHOP, chess.BLACK)) - len(board.pieces(chess.BISHOP, chess.WHITE)))
+  #Pontuação torre
+  points += 5 * (len(board.pieces(chess.ROOK, chess.BLACK)) - len(board.pieces(chess.ROOK, chess.WHITE)))
+  #Pontuação rainha
+  points += 9 * (len(board.pieces(chess.QUEEN, chess.BLACK)) - len(board.pieces(chess.QUEEN, chess.WHITE)))
+  #Pontuação rei
+  points += 100000000 * (len(board.pieces(chess.KING, chess.BLACK)) - len(board.pieces(chess.KING, chess.WHITE)))
+
+  return points
+
 def transformList():
   moves = str(board.legal_moves)
   startingMoves = int(moves.index("("))
   endingMoves = int(moves.index(")"))
-  movementList = list(moves[startingMoves + 1:endingMoves].split(" "))
-  print('\n Jogadas disponiveis: ')
-  for move in movementList:
-      print(move) 
+  return list(moves[startingMoves + 1:endingMoves].split(" "))
+
     
 def minmax(nodes, depth, alpha, beta, player):
     if(depth == 0 or endGame(board)):
@@ -42,56 +58,52 @@ def minmax(nodes, depth, alpha, beta, player):
       
 def copyBoard(fen):
   board = chess.Board(fen)
-  
-        
+          
 def jogadas(movimentos):
   moves = str(board.legal_moves)
   startingMoves = int(moves.index("("))
   endingMoves = int(moves.index(")"))
   movementList = list(moves[startingMoves + 1:endingMoves].split(" "))
-  
-  if(movimentos == 1):
-    print(board.fen())
-    board.push_san("e5")
-    print(board.fen())
-    print(board)
-  elif(movimentos == 2):
-    board.push_san("Nc6")
-    print(board)
-  else:
-    movementList[1]
 
-def Play(movimentos):
+  transformList()
+  movement = input("\n Digite sua jogada IA: ")
+  board.push_san(movement)
+  print(board)
+
+def Play(movimentos, IA):
   
+    print(score(board))
   
     if(not endGame()):
       if(board.is_check()):
         print("---------Check---------")
-
-      try:
-        transformList()
-        movement = input("\n Digite sua jogada: ")
-        board.push_san(movement)
-        print(board)
-        if(movimentos == 3):
-          print(board.fen())
-        if(movimentos == 5):
-          print("Atual: ", board.fen())
-      
-        movimentos += 1
-        print(movimentos)
-        jogadas(movimentos)
-        Play(movimentos)
         
-      except:
-          print('\n Movimento invalido, tente novamente.')
-          Play(movimentos)
-    else:
-      #fim de jogo
+      if(IA):
+        try:
+          jogadas(movimentos)
+          IA = False
+        except:
+          Play(movimentos, IA)
+      else:
+        try:
+          print('\n Jogadas disponiveis: ')
+          move = transformList()
+          for move in movementList:
+            print(move) 
+          movement = input("\n Digite sua jogada: ")
+          board.push_san(movement)
+          movimentos += 1
+          IA = True
+        except:
+            print('\n Movimento invalido, tente novamente.')
+          
       print(board)
+      Play(movimentos, IA)    
+    else:
+      print("---------Check - Mate---------")
 
 
 board = chess.Board()
 movimentos = 0
 
-Play(movimentos)
+Play(movimentos, False)
